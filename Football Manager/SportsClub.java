1c80c8f0d3ac416cb79ba60da6d199b014fc9cc0
Musfira89 <84585739+Musfira89@users.noreply.github.com>
2021-12-24 13:17:05 +0500

    Update SportsClub.java

--- Football Manager/SportsClub.java
+++ Football Manager/SportsClub.java
@@ -2,43 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//import package
 package com.football;
 
 /**
  *
  * @author New
  */
+// Abstract class
 public abstract class SportsClub {
-    private String name;
-    private String location;
-    private String statistics;
+    
+    //private data members
+    private String name;        
+    private String location
+    private String statistics;    
     
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)        //String equals() method overrides the equals() method of the Object class
+    {   
         return this.name.equals(((SportsClub)o).name);
     }
     
-    
+    //getter method for name 
     public String getName() {
         return name;
     }
     
+    //getter method for location
     public String getLocation() {
         return location;
     }
     
+    //getter method for statistics
     public String getStatistics() {
         return statistics;
     }
     
+    //setter method for locatioin
     public void setLocation(String s) {
         this.location = s;
     }
     
+    //setter method for name
     public void setName(String s) {
         this.name = s;
     } 
     
+    //setter method for statistics
     public void setStatistics(String s) {
         this.statistics = s;
     }   
