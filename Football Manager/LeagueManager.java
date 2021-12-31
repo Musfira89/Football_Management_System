a9ca9db8e891d6dc2cd0fb6c2f86db2b6ebe6729
Musfira89 <84585739+Musfira89@users.noreply.github.com>
2021-12-31 18:42:10 +0500

    Update LeagueManager.java

--- Football Manager/LeagueManager.java
+++ Football Manager/LeagueManager.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// import packages here
 package com.football;
 
 /**
  *
  * @author New
  */
+// Interface is used to achieve abstraction.
 public interface LeagueManager {
     
 }
